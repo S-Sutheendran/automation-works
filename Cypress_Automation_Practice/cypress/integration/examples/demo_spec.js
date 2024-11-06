dd5442fd93550647e17e8e7537fe0a1018118947
S-Sutheendran <sutheendransrinivas@gmail.com>
2024-11-06 18:16:07 +0530

    android_app_development

---
+++ Cypress_Automation_Practice/cypress/integration/examples/demo_spec.js
@@ -0,0 +1,6 @@
+describe("My first test suite", function () {
+  it("My first test case scenario", function () {
+    //Test Steps will be inside this function
+    cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+  });
+});
