a0c179c4e9baa5aa2403e77f050e4cfd43285704
S-Sutheendran <sutheendransrinivas@gmail.com>
2026-02-04 20:48:58 +0530

    removal-unwanted-codes

---
+++ Selenium-Python-Practice/Python-Practice/first_demo.py
@@ -0,0 +1,15 @@
+# Demo print statement
+print('Hello World')
+
+# Single Line Comment
+
+a = 3 # This is a variable
+print(a)
+
+print(type(a))
+
+str = "My name is John"
+print(str)
+
+b, c, d = 5, 10.5, "Smith"
+print(b,c,d)
